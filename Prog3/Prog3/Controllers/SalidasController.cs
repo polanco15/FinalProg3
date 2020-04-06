026becd6842ee0f6855babfd8631b71a2b9ff6cb
polanco15 <araidepolanco@gmail,com>
2020-04-06 06:12:56 -0400

    salidas

---
+++ Prog3/Prog3/Controllers/SalidasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Prog3;
+
+namespace Prog3.Controllers
+{
+    public class SalidasController : Controller
+    {
+        private FinalPrEntities db = new FinalPrEntities();
+
+        // GET: Salidas
+        public ActionResult Index()
+        {
+            var salidas = db.Salidas.Include(s => s.Producto);
+            return View(salidas.ToList());
+        }
+
+        // GET: Salidas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salidas salidas = db.Salidas.Find(id);
+            if (salidas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salidas);
+        }
+
+        // GET: Salidas/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdProducto = new SelectList(db.Producto, "IdProducto", "Nombre");
+            return View();
+        }
+
+        // POST: Salidas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdSalida,IdProducto,Cantidad,Fecha")] Salidas salidas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Salidas.Add(salidas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdProducto = new SelectList(db.Producto, "IdProducto", "Nombre", salidas.IdProducto);
+            return View(salidas);
+        }
+
+        // GET: Salidas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salidas salidas = db.Salidas.Find(id);
+            if (salidas == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdProducto = new SelectList(db.Producto, "IdProducto", "Nombre", salidas.IdProducto);
+            return View(salidas);
+        }
+
+        // POST: Salidas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdSalida,IdProducto,Cantidad,Fecha")] Salidas salidas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(salidas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdProducto = new SelectList(db.Producto, "IdProducto", "Nombre", salidas.IdProducto);
+            return View(salidas);
+        }
+
+        // GET: Salidas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salidas salidas = db.Salidas.Find(id);
+            if (salidas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salidas);
+        }
+
+        // POST: Salidas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Salidas salidas = db.Salidas.Find(id);
+            db.Salidas.Remove(salidas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
