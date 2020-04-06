76a4b214c993f3de82457854a3eaceb70816eeb0
polanco15 <araidepolanco@gmail,com>
2020-04-06 06:11:53 -0400

    facturas

---
+++ Prog3/Prog3/Controllers/FacturasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Prog3;
+
+namespace Prog3.Controllers
+{
+    public class FacturasController : Controller
+    {
+        private FinalPrEntities db = new FinalPrEntities();
+
+        // GET: Facturas
+        public ActionResult Index()
+        {
+            var factura = db.Factura.Include(f => f.Cliente).Include(f => f.Producto);
+            return View(factura.ToList());
+        }
+
+        // GET: Facturas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Factura factura = db.Factura.Find(id);
+            if (factura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(factura);
+        }
+
+        // GET: Facturas/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdCliente = new SelectList(db.Cliente, "IdCliente", "Nombre");
+            ViewBag.IdProducto = new SelectList(db.Producto, "IdProducto", "Nombre");
+            return View();
+        }
+
+        // POST: Facturas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdFactura,IdProducto,IdCliente,SubTotal,ITBIS,Total")] Factura factura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Factura.Add(factura);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdCliente = new SelectList(db.Cliente, "IdCliente", "Nombre", factura.IdCliente);
+            ViewBag.IdProducto = new SelectList(db.Producto, "IdProducto", "Nombre", factura.IdProducto);
+            return View(factura);
+        }
+
+        // GET: Facturas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Factura factura = db.Factura.Find(id);
+            if (factura == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdCliente = new SelectList(db.Cliente, "IdCliente", "Nombre", factura.IdCliente);
+            ViewBag.IdProducto = new SelectList(db.Producto, "IdProducto", "Nombre", factura.IdProducto);
+            return View(factura);
+        }
+
+        // POST: Facturas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdFactura,IdProducto,IdCliente,SubTotal,ITBIS,Total")] Factura factura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(factura).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdCliente = new SelectList(db.Cliente, "IdCliente", "Nombre", factura.IdCliente);
+            ViewBag.IdProducto = new SelectList(db.Producto, "IdProducto", "Nombre", factura.IdProducto);
+            return View(factura);
+        }
+
+        // GET: Facturas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Factura factura = db.Factura.Find(id);
+            if (factura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(factura);
+        }
+
+        // POST: Facturas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Factura factura = db.Factura.Find(id);
+            db.Factura.Remove(factura);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
